a15909c5d436560e892d5c22222ba9fbbf43f660
Peter Morjan <pmorjan@gmail.com>
2014-12-23 20:57:50 +0100

    pwcalc.go: don't echo secret

--- pwcalc.go
+++ pwcalc.go
@@ -4,20 +4,30 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/bgentry/speakeasy"
 )
 
 func main() {
-	var alias, secret string
-
-	fmt.Print("enter alias: ")
+	var alias string
+	var length int
+	fmt.Print("# enter alias: ")
 	fmt.Scanf("%s", &alias)
-	fmt.Print("enter secret: ")
-	fmt.Scanf("%s", &secret)
+	secret, err := speakeasy.Ask("# enter secret: ")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Print("# enter length: ")
+	fmt.Scanf("%d", &length)
+	if length == 0 {
+		length = 16
+	}
 
 	str := []byte(strings.TrimSpace(secret) + strings.TrimSpace(alias))
 	hash := fmt.Sprintf("%s", sha1.Sum(str))
-	base64 := base64.StdEncoding.EncodeToString([]byte(hash))[0:16]
-
-	fmt.Println(base64)
+	base64 := base64.StdEncoding.EncodeToString([]byte(hash))[0:length]
+	fmt.Printf("\n%s\n", base64)
 }
